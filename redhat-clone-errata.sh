ee0a120d02b6f4cba0290fc0484b841420d30d37
Franky Van Liedekerke <liedekef@telenet.be>
2013-02-12 15:45:34 +0100

    Update redhat-clone-errata.sh

--- redhat-clone-errata.sh
+++ redhat-clone-errata.sh
@@ -18,7 +18,7 @@ SPACEWALK=127.0.0.1
 # now do the import
 # change the channel parameter to your liking
 #   and add the option "--redhat-channel <RHN channel name>" if the channel name if not the same as in your spacewalk server
-/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-i386-server-5 --os-version 5 --publish --redhat --rhn-proxy=xxx --redhat-startfromprevious twoweeks --quiet
-/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-i386-server-6 --os-version 6 --publish --redhat --rhn-proxy=xxx --redhat-startfromprevious twoweeks --quiet
-/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-x86_64-server-5 --os-version 5 --publish --redhat --rhn-proxy=xxx --redhat-startfromprevious twoweeks --quiet
-/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-x86_64-server-6 --os-version 6 --publish --redhat --rhn-proxy=xxx --redhat-startfromprevious twoweeks --quiet
+/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-i386-server-5 --os-version 5 --publish --redhat --rhn-proxy=xxx --startfromprevious twoweeks --quiet
+/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-i386-server-6 --os-version 6 --publish --redhat --rhn-proxy=xxx --startfromprevious twoweeks --quiet
+/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-x86_64-server-5 --os-version 5 --publish --redhat --rhn-proxy=xxx --startfromprevious twoweeks --quiet
+/sbin/ya-errata-import.pl --server $SPACEWALK --channel rhel-x86_64-server-6 --os-version 6 --publish --redhat --rhn-proxy=xxx --startfromprevious twoweeks --quiet
