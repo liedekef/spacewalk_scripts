b09e2f6dc959371b296c6039921d6cc3a76d89e7
Franky Van Liedekerke <liedekef@telenet.be>
2013-02-14 12:44:36 +0100

    Update centos-clone-errata.sh

--- centos-clone-errata.sh
+++ centos-clone-errata.sh
@@ -2,19 +2,38 @@
 # Processes CentOS Errata and imports it into Spacewalk
 
 # IMPORTANT: read through this script, it's more of a guidance than something fixed in stone
-# This script is meant to be run every day from cron or so. If you don't want to do this every day,
-# you might want to change the code below marked with "EVERY DAY"
+# This script is meant to be run every day from cron or so.
+# The things to edit are between "#### EDIT SECTION x HERE ####" and "#### STOP EDITING SECTION x HERE ####" lines
+#    there are 2 sections: one for config variables, one for the different calls to the real script and spacewalk channels
 
 # set fixed locale
 export LC_ALL=C
 export LANG=C
 
+#### EDIT SECTION 1 HERE ####
 # Set your spacewalk server
 SPACEWALK=127.0.0.1
 
 # The number of digests to download (normally there is max 1 errata per day)
 # Since the digests are normally only downloaded for 1 month, any number not in the range 1-28 (Februari) makes no sence
 NBR_DIGESTS=5
+
+# Set usernames and passwords. You have some options here:
+# 1) Either define the environment variables here:
+#   export SPACEWALK_USER=my_username
+#   export SPACEWALK_PASS=my_passwd
+#   export RHN_USER=my_rhn_username
+#   export RHN_PASS=my_rhn_password
+# 2) Set them on the commandline (but I don't recommend it)
+# 3) Set them in a separate cfg file and source it (like done below)
+. /etc/ya-errata-import.cfg
+
+# wget needs a proxy? Then set these (or set them also in the config file you used above)
+export http_proxy=
+export https_proxy=
+
+#### STOP EDITING SECTION 1 HERE ####
+
 if [ $NBR_DIGESTS -lt 1 ]; then
    $NBR_DIGESTS=1;
 fi
@@ -29,9 +48,6 @@ rm -f $ERRATADIR/* >/dev/null 2>&1
 
 (
    cd $ERRATADIR
-   # wget needs a proxy? Then set these
-   export http_proxy=
-   export https_proxy=
 
    eval `exec /bin/date -u +'yearmon=%Y-%B day=%d'`
    # for the first day of the month: also consider last month
@@ -52,15 +68,7 @@ rm -f $ERRATADIR/* >/dev/null 2>&1
    #wget --no-cache -q -O- http://lists.centos.org/pipermail/centos/$DATE/date.html| grep "CentOS-announce Digest" |tail -n 5 |cut -d"\"" -f2|xargs -n1 -I{} wget -q http://lists.centos.org/pipermail/centos/$DATE/{}
 )
 
-# Set usernames and passwords. You have some options here:
-# 1) Either define the environment variables here:
-#   export SPACEWALK_USER=my_username
-#   export SPACEWALK_PASS=my_passwd
-#   export RHN_USER=my_rhn_username
-#   export RHN_PASS=my_rhn_password
-# 2) Set them on the commandline (but I don't recommend it)
-# 3) Set them in a separate cfg file and source it (like done below)
-. ./ya-errata-import.cfg
+#### EDIT SECTION 2 HERE ####
 
 # now do the import based on the wget results
 # !!!! change the channel parameter to your liking
@@ -72,5 +80,7 @@ rm -f $ERRATADIR/* >/dev/null 2>&1
 #   and add the option "--redhat-channel <RHN channel name>" if the channel name if not the same as in your spacewalk server
 #/sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-6 --os-version 6 --publish --get-from-rhn --rhn-proxy=xxx --quiet
 #/sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-5 --os-version 5 --publish --get-from-rhn --rhn-proxy=xxx --quiet
+
+#### STOP EDITING SECTION 2 HERE ####
 
 rm -f $ERRATADIR/*
