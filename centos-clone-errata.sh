44bf9b37702525759cc48ce97ea6963f4ed9a031
root <root@spacewalk.prod.ch3.s.com>
2018-06-05 12:24:23 -0500

    Adding a couple other mailing lists (CentOS-Announce and CentOS-CR-Announce)

--- centos-clone-errata.sh
+++ centos-clone-errata.sh
@@ -11,6 +11,34 @@ export LC_ALL=C
 export LANG=C
 
 #### EDIT SECTION 1 HERE ####
+
+SCRIPTNAME=$(basename ${0})
+THISDATE=$(date +%F)
+LOGPATH='/tmp/errata-import'
+LOGFILE="${LOGPATH}/${SCRIPTNAME}.log"
+LOCKFILE="/var/run/${SCRIPTNAME}.lck"
+SUPPORTEMAIL="me@you.com"
+# UPDATE THESE TO YOUR CHANNEL NAMES
+CHANNELSTOCONSIDER="centos5-x86_64-all
+centos6-x86_64-all
+centos7-x86_64-all"
+
+# Trap a Ctrl-C to clean up the lock file
+trap ctrl_c INT TERM
+function ctrl_c(){
+        echo '*** TRAPPED CTRL-C ***' >> "${LOGFILE}"
+        rm "${LOCKFILE}" && exit 1
+}
+
+if [ -f "${LOCKFILE}" ]; then
+        echo "Lock file exists at ${LOCKFILE}, exiting!" | tee -a "${LOGFILE}"
+        exit 1
+else
+        echo "${SCRIPTNAME} started on $(date)" | tee -a "${LOCKFILE}"
+fi
+
+echo "${THISDATE} - Starting ${SCRIPTNAME}"
+
 # Set your spacewalk server
 SPACEWALK=127.0.0.1
 
@@ -62,17 +90,47 @@ ERRATADIR=/tmp/centos-errata
 		| sed -n 's|.*"\([^"]*\)".*CentOS-announce Digest.*|'"$d/\\1|p"
      done
    } |	tail -n $NBR_DIGESTS | xargs -n1 -I{} wget -q $listurl/{}
+   # Also scrape the actual CentOS-announce mailing list since the digests are not completely reliable
+   listurl=https://lists.centos.org/pipermail/centos-announce
+   { for d in $yearmon; do
+ 	  wget --no-cache -q -O- $listurl/$d/date.html \
+ 		| sed -n 's|.*"\([^"]*\)".*\[CentOS-announce\].*CE.A.*|'"$d/\\1|p"
+     done
+   } | tail -n $NBR_DIGESTS | xargs -n1 -I{} wget -q $listurl/{} 
+
+   # Also scrape the CentOS-CR-announce mailing list since the digests are not completely reliable
+   listurl=https://lists.centos.org/pipermail/centos-cr-announce
+   { for d in $yearmon; do
+          wget --no-cache -q -O- $listurl/$d/date.html \
+                | sed -n 's|.*"\([^"]*\)".*\[CentOS-CR-announce\].*CE.A.*|'"$d/\\1|p"
+     done
+   } | tail -n $NBR_DIGESTS | xargs -n1 -I{} wget -q $listurl/{}
 
    # the ye old simple way, left as an example for reference:
    #wget --no-cache -q -O- https://lists.centos.org/pipermail/centos/$DATE/date.html| grep "CentOS-announce Digest" |tail -n 5 |cut -d"\"" -f2|xargs -n1 -I{} wget -q http://lists.centos.org/pipermail/centos/$DATE/{}
 )
 
+LOGFILE=/var/log/rhn/shc/errata-import/ya-errata-import.sh.log
+REDHATERRATAURL='https://www.redhat.com/security/data/oval/com.redhat.rhsa-all.xml'
+OVALPATH="${ERRATADIR}/com.redhat.rhsa-all.xml"
+/usr/bin/wget -O"${OVALPATH}" -N "${REDHATERRATAURL}" >>"${LOGFILE}" 2>&1
+if [ $? -ne 0 ]; then
+        echo "Download of ${REDHATERRATAURL} failed!">>"${LOGFILE}"
+        echo -e "${THISDATE} - ${0} failed!\n" | mailx -a "${LOGFILE}" -s "Spacewalk errata download error!" "${SUPPORTEMAIL}"
+fi
+
 #### EDIT SECTION 2 HERE ####
 
 # now do the import based on the wget results
 # !!!! change the channel parameter to your liking
-/sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-6 --os-version 6 --publish --quiet
-/sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-5 --os-version 5 --publish --quiet
+# /sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-6 --os-version 6 --publish --quiet
+# /sbin/ya-errata-import.pl --erratadir=$ERRATADIR --server $SPACEWALK --channel centos-x86_64-server-5 --os-version 5 --publish --quiet
+
+for CHANNEL in ${CHANNELSTOCONSIDER}; do
+	# Modify below to extract the OSVER from your channel name (you are using standardized names, right?), or use the above method of one line per import operation
+	OSVER=$(echo "${CHANNEL}" | awk -F- '{print $1}' | sed 's/[[:alpha:]]//g')
+	/usr/local/bin/ya-errata-import.pl --erratadir="${ERRATADIR}" --server "${SPACEWALK}" --channel "${CHANNEL}" --os-version "${OSVER}" --publish --rhsa-oval "${OVALPATH}" >>"${LOGFILE}" 2>&1
+done
 
 # OR do the import and get extra errata info from redhat if possible
 # change the channel parameter to your liking
@@ -83,3 +141,4 @@ ERRATADIR=/tmp/centos-errata
 #### STOP EDITING SECTION 2 HERE ####
 
 rm -f $ERRATADIR/*
+rm "${LOCKFILE}"
